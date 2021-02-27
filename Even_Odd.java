dfb3e781b5970245f02527ce10c7038da4d0a1bf
AnshuMitra95 <65309773+AnshuMitra95@users.noreply.github.com>
2021-02-27 19:21:53 +0530

    Find Even and Odd Number

---
+++ Even_Odd.java
@@ -0,0 +1,18 @@
+//Check given no. is even or odd
+import java.util.*;
+class Even_Odd
+{
+   public static void main(String a[])
+    {
+	   int n;
+	   Scanner ob=new Scanner(System.in);
+       System.out.println("Enter a number");
+       n=ob.nextInt();
+       if(n%2==0)
+	        System.out.println("Even number");
+	   else
+             System.out.println("Odd number");
+    }
+}
+	
+         	   
